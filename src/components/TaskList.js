bdb3f2c1c38a5b9369577038cb0f512c2cb445cb
Reid Angwin <rangwin@gmail.com>
2018-02-23 04:09:55 -0500

    Fixed issues that were causing console errors:       - Instance of 'class' rather than 'className' in TaskList jsx       - Added key to the tr and nested td in the map function that renders tasks       - Removed re-base and did a fresh install with 'yarn remove re-base', 'yarn       add re-base'. While still functional, old version of firebase was throwing       console errors. (see https://github.com/firebase/firebase-js-sdk/issues/517)

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import App from './App' 
 import { Button } from 'react-bootstrap';
 
 class TaskList extends Component {
@@ -12,20 +11,23 @@ class TaskList extends Component {
   render() {
     return (
       <div className="row-offcanvas row-offcanvas-left">
-        <div id="sidebar" class="sidebar-offcanvas">
+        <div id="sidebar" className="sidebar-offcanvas">
           <div className="col-md-12">
             <h3>Task History</h3>
             <form ref={(input) => this.taskForm = input} className="task-edit" onSubmit={(e) => this.createTask(e)}>
               <input ref={(input) => this.task = input} type="text" placeholder="Task" />
               <Button type="submit">Add Task</Button>
             </form>
-              <table className="table table-striped table-dark">
-                <tbody>
-                  {Object.keys(this.props.taskList).reverse().map( (key) =>
-                    <tr key={key}><td>{this.props.taskList[key]}</td></tr>
-                  )};
-                </tbody>
-              </table>
+            <table className="table table-striped table-dark">
+              <tbody>
+                {
+                  Object
+                    .keys(this.props.taskList)
+                    .reverse()
+                    .map(key => <tr key={key}><td key={{key}}>{this.props.taskList[key]}</td></tr>)
+                }
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import App from './App' 
 import { Button } from 'react-bootstrap';
 
 class Timer extends Component {
